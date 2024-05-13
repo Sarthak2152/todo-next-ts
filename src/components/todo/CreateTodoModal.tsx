5a1e642c4b83fd1b41803d15c93abd26c122711d
Sarthak <sarthakapoor2152@gmail.com>
2024-05-13 12:58:43 +0530

    Fixed bug

--- src/components/todo/CreateTodoModal.tsx
+++ src/components/todo/CreateTodoModal.tsx
@@ -22,7 +22,7 @@ export const CreateTodoModal = () => {
         <div className="flex flex-col items-center gap-4">
           <Input type="text" placeholder="Title" />
           <Input type="text" placeholder="Description" />
-          <DatePicker />
+          <DatePicker target={undefined} disabled={false} />
         </div>
         <div className="w-full text-center">
           <Button type="submit" className="text-center max-w-sm w-full px-3">
